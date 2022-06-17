449616737c42bc57e6f91c42c9cace0478ed32da
arturzokyolabs <arturzokyolabs@gmail.com>
2022-06-17 19:28:57 +0300

    chore: add ProfileCard component

---
+++ components/ProfileCard/index.tsx
@@ -0,0 +1,35 @@
+import { memo, useState } from "react";
+import Image from "next/image";
+import cn from "classnames";
+import s from "./styles.module.scss";
+
+function ProfileCard() {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className={cn(s.card_main_box, { [s.active]: isActive })}>
+      <div className={s.contentBx}>
+        <div className={s.content}>
+          <h2>
+            Someone Famous <br /> <span>Creative Desinger</span>
+          </h2>
+          <div className={s.imgBx}>
+            <Image
+              src="/user_avatar.webp"
+              alt="user avatar"
+              width={128}
+              height={128}
+              layout="responsive"
+            />
+            <button>Hi👋, its me!</button>
+          </div>
+        </div>
+      </div>
+      <div className={s.toggle} onClick={() => setIsActive((prev) => !prev)}>
+        <span />
+      </div>
+    </div>
+  );
+}
+
+export default memo(ProfileCard);
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { GoLocation } from "react-icons/go";
 import { GiTie } from "react-icons/gi";
 import { useTheme } from "next-themes";
 import { useCallback } from "react";
-import Sandbox from "../pages/sandbox";
+import ProfileCard from "./ProfileCard";
 
 const Sidebar = () => {
   const { theme, setTheme } = useTheme();
@@ -15,7 +15,7 @@ const Sidebar = () => {
 
   return (
     <>
-      <Sandbox/>
+      <ProfileCard/>
       <h3 className="my-4 text-3xl font-medium tracking-wider pt-14 font-kaushan">
         <span className="text-green">Artur </span>
         Zub
@@ -31,7 +31,7 @@ const Sidebar = () => {
         <GiTie className="w-6 h-6" />
         Download Resume
       </a>
-      <div className="flex justify-around w-9/12 mx-auto my-5 text-green-500 md:w-full">
+      <div className="flex justify-around w-9/12 my-5 text-green-500 jmx-auto md:w-full">
         <a href="">
           <AiFillGithub className="w-8 h-8 cursor-pointer" />
         </a>
